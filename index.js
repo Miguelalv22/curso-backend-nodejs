6ab03cd1dbff3d96e9157b3321d8011bf1c85212
Miguel Lopez <miguel.2211.lopez@gmail.com>
2025-07-09 14:17:49 -0600

    Instalacion de Express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("My Store")
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hola server');
+});
+
+app.listen(port, () => {
+  console.log(`Mi port ${port}`);
+});
