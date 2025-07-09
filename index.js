fe2e53683f9d3d03bcf277d601a0cf32abd146af
Miguel Lopez <miguel.2211.lopez@gmail.com>
2025-07-09 14:25:43 -0600

    Routing

--- index.js
+++ index.js
@@ -6,6 +6,29 @@ app.get('/', (req, res) => {
   res.send('Hola server');
 });
 
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola soy la nueva ruta');
+});
+
+app.get('/products', (req, res) => {
+  res.json({
+    name: 'Product 1',
+    price: 120
+  });
+});
+
+app.get('/categories', (req, res) => {
+  res.json({
+    clothes: 'Clothes',
+    electronics: 'Electronics',
+    toys: 'Toys'
+  });
+});
+
+app.get('/home', (req, res) => {
+  res.send('Home Page');
+});
+
 app.listen(port, () => {
   console.log(`Mi port ${port}`);
 });
