62e12161da3ec377713f4c4d7a5e2ee3daf6c021
Miguel Lopez <miguel.2211.lopez@gmail.com>
2025-07-10 13:43:41 -0600

    Put, Patch, Delete

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.get('/', (req, res) => {
   const products = [];
   const { size } = req.query;
-  const limit = size || 10;
+  const limit = size || 5;
 
   for (let i = 0; i < limit; i++) {
     products.push({
@@ -34,8 +34,26 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
   const body = req.body;
   res.json({
-    message: 'create',
+    message: 'created',
     data: body
+  });
+});
+
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: 'updated',
+    data: body,
+    id,
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: 'deleted',
+    id,
   });
 });
 
