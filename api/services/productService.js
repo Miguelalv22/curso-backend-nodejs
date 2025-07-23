0060167836964a3c2e25c053c7becf99ef0206f4
Miguel Lopez <miguel.2211.lopez@gmail.com>
2025-07-23 08:52:08 -0600

    Relacion Uno a Muchos Category/Product

--- api/services/productService.js
+++ api/services/productService.js
@@ -1,6 +1,6 @@
 const { faker } = require('@faker-js/faker');
 const boom = require('@hapi/boom');
-const sequelize = require('../libs/sequelize');
+const { models } = require('../libs/sequelize');
 
 class ProductsService {
   constructor() {
@@ -22,17 +22,14 @@ class ProductsService {
     }
   }
   async create(data) {
-    const newProduct = {
-      id: faker.string.uuid(),
-      ...data
-    }
-    this.products.push(newProduct);
+    const newProduct = await models.Product.create(data);
     return newProduct;
   }
   async find() {
-    const query = 'SELECT * FROM tasks';
-    const [data] = await sequelize.query(query);
-    return data;
+    const products = await models.Product.findAll({
+      include: ['category']
+    });
+    return products;
   }
   async findOne(id) {
     const product = this.products.find(item => item.id === id);
